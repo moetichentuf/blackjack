b6d8b68633b600c5846d4470a76689dc0f06a940
moeti <moetichentuf@hotmail.com>
2020-02-14 11:53:31 +0100

    adding numbers

--- game.php
+++ game.php
@@ -10,6 +10,29 @@ class Blackjack {
     function set_hit($hit) {
         $this->hit = $hit;
 
+        $keepChecking = true;
+        $values = 0;
+        $values2 = 0;
+        $values3 = 0;
+        $valuestotal = 0;
+        while($keepChecking){
+            $values = rand(1, 11);
+            $values2 = rand(1, 11);
+            $values3 = rand(1, 11);
+            $valuestotal = $values + $values2 + $values3;
+            if ($valuestotal === 21){
+                $keepChecking = false;
+            }
+        }
+        echo $values;
+        echo '<br>';
+        echo $values2;
+        echo '<br>';
+        echo $values3;
+        echo "<br>";
+        echo $valuestotal;
+
+
     }
     function get_hit() {
         return $this->hit;
@@ -28,23 +51,12 @@ class Blackjack {
     }
 }
 
-$count=0;
-$i=0;
-$target=99;
-
-while( $i!=$target ){
-    $i=rand(1,100);
-    $count++;
-}
-printf('it took %u attempts to reach %u',$count,$target);
-
-
 
 $apple = new Blackjack();
 $apple->set_hit('');
 $apple->set_stand('0');
 $apple->set_surrender('');
-echo "<button type=\"button\" value=\"Start\" >Hit</button> " . $apple->get_hit();
+echo " <input type=\"submit\" name=\"select\" value=\"select\" onclick=\"select()\" /> " . $apple->get_hit();
 
 echo "<br>";
 echo "<button type=\"button\" value=\"Start\" >Stand</button>" . $apple->get_stand();
